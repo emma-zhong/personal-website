13fed4cc19439f6432902807460ed4f52d468bf5
emma-zhong <emmazhong990@gmail.com>
2026-02-06 02:55:24 -0800

    courses

---
+++ blog/app/components/courses.tsx
@@ -0,0 +1,44 @@
+export default function Courses() {
+    const classes = {
+        'spring 2026': [
+            'cs 168: introduction to the internet: architecture and protocols',
+            'cs 189: introduction to machine learning',
+            'data 140: probability for data science',
+            'music 141a: philharmonia orchestra',
+            'cs 370: adaptive instruction methods in computer science',
+            'cs 399: cs 61a ta'
+        ],
+        'fall 2025': [
+            'cs 70: discrete mathematics and probability theory',
+            'data 101: data engineering',
+            'eecs 127: optimization models in engineering',
+            'music 141a: philharmonia orchestra',
+            'cs 197: cs 61a tutor'
+        ],
+        'summer 2025': [
+            'cs 61c: great ideals of computer architecture',
+            'cs 197: cs 61a tutor'
+        ],
+        'spring 2025': [
+            'cs 61b: data structures',
+            'data 100: principles and techniques of data science',
+            'data 104: human contexts and ethics of data',
+            'integbi 35ac: human biological variation',
+            'music 141: university symphony orchestra',
+            'cs 365: cs 61a academic intern',
+            'ugis 192c: supervised research: biological sciences'
+        ],
+        'fall 2024': [
+            'astron 10: introduction to general astronomy',
+            'cs 61a: structure and interpretation of computer programs',
+            'data 8: foundations of data science',
+            'music 141: university symphony orchestra'
+        ],
+        'transferred courses': [
+            'multivariable calculus',
+            'linear algebra and differential equations',
+            'introduction to sociology'
+        ]
+    }
+} as const
+
---
+++ blog/app/courses/page.tsx
@@ -0,0 +1,5 @@
+import Courses from 'app/components/courses'
+
+export default function CoursesPage() {
+  return <Courses />
+}
