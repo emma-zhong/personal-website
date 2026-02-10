2b3eda3143c6c313a966c64013a7e2d56e998e53
emma-zhong <emmazhong990@gmail.com>
2026-02-09 18:25:48 -0800

    removing github page soncfig

--- blog/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true, // required for GitHub Pages
-  },
-};
-
-module.exports = nextConfig;
