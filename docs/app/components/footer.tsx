34b4813b0dd811e6fa3d96e808ee964280f13601
emma-zhong <emmazhong990@gmail.com>
2026-06-04 18:58:13 -0700

    commenting out spotify on footer

--- docs/app/components/footer.tsx
+++ docs/app/components/footer.tsx
@@ -52,7 +52,7 @@ export default function Footer() {
             <p className="ml-2 h-7">github</p>
           </a>
         </li>
-        <li>
+        {/* <li>
           <a
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
@@ -62,7 +62,7 @@ export default function Footer() {
             <ArrowIcon />
             <p className="ml-2 h-7">spotify</p>
           </a>
-        </li>
+        </li> */}
       </ul>
       {/* <p className="mt-8 text-neutral-600 dark:text-neutral-300">
         last updated {new Date().getFullYear()}
