d15f096ce57e7f704c438731a59aab33b22a0632
emma-zhong <emma_zhong@berkeley.edu>
2026-06-07 17:45:28 -0700

    github commit history  (#2)
    * gh
    
    * cleaning code

--- docs/app/misc/page.tsx
+++ docs/app/misc/page.tsx
@@ -1,5 +1,18 @@
-import Misc from 'app/components/misc'
+import { GithubActivitySection } from './github-activity'
+import { LetterboxdSection } from './letterboxd'
+import { SpotifySection } from './spotify'
 
 export default function MiscPage() {
-  return <Misc />
+  return (
+    <section>
+      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">
+        miscellaneous
+      </h1>
+      <div className="space-y-10">
+        <GithubActivitySection />
+        <LetterboxdSection />
+        <SpotifySection />
+      </div>
+    </section>
+  )
 }
